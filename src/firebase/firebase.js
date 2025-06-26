dc316d95f762b4a71dae90bc782c22b121b0e80a
Shubham Adhav <adhavshubham002@gmail.com>
2025-06-26 19:16:27 +0530

    some changes with firebase code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdPuQGHTSRDvipjS83ftQLjVaQCBA2jbI",
-    authDomain: "seminar-hall-booking-1892c.firebaseapp.com",
-    projectId: "seminar-hall-booking-1892c",
-    storageBucket: "seminar-hall-booking-1892c.firebasestorage.app",
-    messagingSenderId: "346170529410",
-    appId: "1:346170529410:web:e462108acdadab7111869b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
